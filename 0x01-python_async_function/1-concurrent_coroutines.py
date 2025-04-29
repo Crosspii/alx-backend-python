358ddc5eb2d0427f85982ca13a743db486bc5ab4
Aymen Hommani <crosspieloo@gmail.com>
2025-04-29 04:52:43 +0100

    fixing styling in task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3  
-""" concurrent coroutine that spanws wiat_rand n times. """
+#!/usr/bin/env python3
+"""Concurrent coroutine that spawns wait_random n times."""
 import asyncio
 from typing import List
 
@@ -7,7 +7,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ spawns a wait_random n times with max_delay and return list of delays in ascending order.
+    """Spawn wait_random n times with max_delay and return list of delays
+    in ascending order.
     """
     delays = []
     for _ in range(n):
