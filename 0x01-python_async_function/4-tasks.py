08f1c43d57391988bf84ab75482eb22a0501bdc2
Aymen Hommani <crosspieloo@gmail.com>
2025-04-29 04:54:37 +0100

    fixing task 4 styling

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Spawn task_wait_random n times and return list of delays."""
+"""Spawn task_wait_random n times and return list of delays."""
 import asyncio
 from typing import List
 
@@ -7,7 +7,9 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn task_wait_random n times and return a list of all dealys in ascending order."""
+    """Spawn task_wait_random n times and return list of all delays
+    in ascending order.
+    """
     tasks = []
     for _ in range(n):
         tasks.append(task_wait_random(max_delay))
